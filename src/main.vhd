96c81e2a8f3f6356f57f600f4d74862285d7ada8
qhb21126 <qhb21126@EEE-R446-38>
2023-03-16 08:38:11 +0000

    Modified main.

--- src/main.vhd
+++ src/main.vhd
@@ -9,15 +9,21 @@ end package body;
 --------------------------------------------- Output type declarations
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
 library work;
 use work.display.all;
 
 entity main is
 GENERIC(
+S       : std_logic_vector:= "1011010";
+H       : std_logic_vector:= "0010111";
+U       : std_logic_vector:= "0011100";
+T       : std_logic_vector:= "0001111";
+
 CLK_FREQ : INTEGER := 50_000_000);
 PORT(
 SWITCH   :  IN std_logic_vector(5 downto 0);
-TRY,CLK  :  IN STD_LOGIC_VECTOR;
+TRY,CLK  :  IN STD_LOGIC;
 LEDs     : OUT STD_LOGIC_VECTOR(15 DOWNTO 0);
 SEGS     : OUT SEVEN_SEGMENT(0 TO 3));
 
@@ -25,28 +31,45 @@ end main;
 
 architecture Behavioral of main is
 TYPE STATE IS (LOCKED, UNLOCKED,WARNING);
-SIGNAL SWITCH_DEB : STD_LOGIC_VECTOR(5 DOWNTO 0):
+SIGNAL SWITCH_DEB : STD_LOGIC_VECTOR(5 DOWNTO 0);
 SIGNAL TRY_DEB : STD_LOGIC;
+SIGNAL POSITION : STATE := LOCKED;
 
-
-END COMPONENT
 
 begin
 
 DEB_GEN: FOR I IN 5 DOWNTO 0 GENERATE
 
-DEB_ELEM : ENTITY WORK.DEBOUNCER
+DEB_ELEM : ENTITY WORK.DEBOUNCER 
 PORT MAP(DIN => SWITCH(I),DOUT => SWITCH_DEB(I),CLK=>CLK);
 END GENERATE;
 
-CASE STATE IS 
+PROCESS(CLK)
+BEGIN
+------------------------------------------------
+CASE (POSITION) IS 
 
 WHEN LOCKED =>
-
+LEDS <= (OTHERS => '0'); --LIGHTS OFF
+                         -- DOOR READS "LOCK"
+SEGS(0) <= S;  
+SEGS(1) <= h;  
+SEGS(2) <= u;  
+SEGS(3) <= t; 
+            
+------------------------------------------------
 WHEN UNLOCKED =>
 
+LEDS <= (OTHERS => '0');
+SEGS(0) <= O;  
+SEGS(1) <= p; 
+SEGS(2) <= e; 
+SEGS(3) <= n; 
+------------------------------------------------
 WHEN WARNING =>
 
-END CASE;
 
+
+END CASE;
+END PROCESS;
 end Behavioral;
